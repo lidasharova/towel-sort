752cbd808615ac220bfa1a0bedbbe814180e99d0
lidasharova <Lida050496@yandex.ru>
2023-01-24 22:42:20 +0700

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+
+
+module.exports = function towelSort(matrix) {
+   let arr = matrix.reduce((sum, cur, i) => {
+        return sum.concat(!(i % 2) ? cur : cur.reverse());
+    }, []);
+    return arr;
+};
