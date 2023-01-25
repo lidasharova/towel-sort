a5d7ce7bfd9acc735049804bb0049c850eccfb80
lidasharova <Lida050496@yandex.ru>
2023-01-25 21:20:16 +0700

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 module.exports = function towelSort(matrix) {
     let newArray = [];
-    if (!matrix.length === 0) {
+    if (matrix == undefined || matrix.length === 0) {
+        return [];
+    } else {
         for (let i = 0; i < matrix.length; i++) {
             if (i % 2 === 1) {
-                newArray.concat(matrix[i].reverse());
+                newArray = newArray.concat(matrix[i].reverse());
             } else {
-                newArray.concat(matrix[i]);
+                newArray = newArray.concat(matrix[i]);
             }
-            return newArray;
         }
-    } else {
-        return [];
+        return newArray;
     }
 };
+
+//2 вариант
+//     let newArray = [];
+//     if (matrix !== undefined && matrix.length !== 0) {
+//         for (let i = 0; i < matrix.length; i++) {
+//             if (i % 2 === 1) {
+//                 newArray = newArray.concat(matrix[i].reverse());
+//             } else {
+//                 newArray = newArray.concat(matrix[i]);
+//             }
+//         }
+//         return newArray;
+//     } else {
+//         return [];
+//     }
+// };
